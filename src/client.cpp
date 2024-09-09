c8e162ae7ae6ff90e049086f8408afb92152750a
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2024-09-09 16:21:41 -0500

    Initial commit
    Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>

---
+++ src/client.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <grpcpp/grpcpp.h>
+#include "text_update.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using text_update::TextUpdateService;
+using text_update::TextUpdateRequest;
+using text_update::TextUpdateResponse;
+
+class TextUpdateClient {
+public:
+    TextUpdateClient(std::shared_ptr<Channel> channel)
+        : stub_(TextUpdateService::NewStub(channel)) {}
+
+    void StreamTextUpdates(const std::string& client_id) {
+        ClientContext context;
+        TextUpdateRequest request;
+        request.set_client_id(client_id);
+
+        std::unique_ptr<grpc::ClientReader<TextUpdateResponse>> reader(
+            stub_->StreamTextUpdates(&context, request));
+
+        TextUpdateResponse response;
+        while (reader->Read(&response)) {
+            std::cout << "Received update: " << response.updated_text() << std::endl;
+        }
+
+        Status status = reader->Finish();
+        if (status.ok()) {
+            std::cout << "StreamTextUpdates rpc succeeded." << std::endl;
+        } else {
+            std::cout << "StreamTextUpdates rpc failed." << std::endl;
+        }
+    }
+
+private:
+    std::unique_ptr<TextUpdateService::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+    TextUpdateClient client(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+    client.StreamTextUpdates("Client1");
+    return 0;
+}
---
+++ src/server.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <grpcpp/grpcpp.h>
+#include "text_update.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerWriter;
+using grpc::Status;
+using text_update::TextUpdateService;
+using text_update::TextUpdateRequest;
+using text_update::TextUpdateResponse;
+
+class TextUpdateServiceImpl final : public TextUpdateService::Service {
+public:
+    Status StreamTextUpdates(ServerContext* context, const TextUpdateRequest* request,
+                             ServerWriter<TextUpdateResponse>* writer) override {
+        std::cout << "Received request from client: " << request->client_id() << std::endl;
+        
+        for (int i = 0; i < 5; ++i) {
+            TextUpdateResponse response;
+            response.set_updated_text("Update " + std::to_string(i + 1) + " for " + request->client_id());
+            
+            if (!writer->Write(response)) {
+                return Status::CANCELLED;
+            }
+            
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+        
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    TextUpdateServiceImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+}
+
+int main(int argc, char** argv) {
+    RunServer();
+    return 0;
+}
