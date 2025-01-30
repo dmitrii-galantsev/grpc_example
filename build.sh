01645ceda23a35fa31adb929c5249fa8e7756862
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2025-01-30 17:02:12 +0000

    Update build.sh
    Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-CC=${CC:-clang}
-CXX=${CXX:-clang++}
+set -eu
+
+GRPC_ROOT=${GRPC_ROOT:-/opt/grpc}
+if ! test -e "$GRPC_ROOT"; then
+    tput bold setaf 3 bold; echo "[$GRPC_ROOT] does not exist, trying another dir"; tput sgr0
+    GRPC_ROOT=/opt/grpc_clang
+fi
+if ! test -e "$GRPC_ROOT"; then
+    tput bold setaf 1 bold; echo "[$GRPC_ROOT] does not exist"; tput sgr0
+    exit 1
+fi
 
 clean() {
     rm -rf build
@@ -9,9 +18,13 @@ clean() {
 }
 
 build() {
+    tput setaf 6 bold; echo -e "configuring project.."; tput sgr0
     cmake -B build \
         -DCMAKE_BUILD_TYPE=Debug \
-        -DCMAKE_PREFIX_PATH=/opt/grpc_clang/
+        -DCMAKE_PREFIX_PATH="$GRPC_ROOT"
+    sleep 0.5
+    tput setaf 6 bold; echo -e "building project.."; tput sgr0
+    make -C build -j "$(nproc)"
 }
 
 clean
